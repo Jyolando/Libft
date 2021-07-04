c9cd1c6e4f6c7fcdb74e238189b0c0bce9fadf7b
Spokich <spokidzn@gmail.com>
2021-07-04 15:15:41 +0300

    header w/ft_memset

---
+++ include/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyolando <jyolando@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/04 13:57:27 by jyolando          #+#    #+#             */
+/*   Updated: 2021/07/04 13:57:27 by jyolando         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_strlen(char *str);
+
+#endif
