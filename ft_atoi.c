f71bd1e2b668bfb1df68cd185e4d4f2e59ac3304
Spokich <spokidzn@gmail.com>
2021-08-17 22:30:37 +0300

    atoi rework

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,6 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+static int ft_isspace(char c)
+{
+    if (c == 32 || (c >= 9 && c <= 13))
+        return (1);
+    return (0);
+}
+
 int	ft_atoi(const char *str)
 {
 	int	res;
@@ -17,7 +24,7 @@ int	ft_atoi(const char *str)
 
 	neg = 1;
 	res = 0;
-	while ((*str == ' ' || *str == '\t') && *str)
+	while (ft_isspace(*str) == 1)
 		str++;
 	if (*str == '-')
 		neg = -neg;
